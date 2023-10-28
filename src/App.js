50272c996b14f650ba5ffcd5304a60fe03528116
TinPham626 <timp72852@gmail.com>
2023-10-27 17:10:31 -0700

    Build Day Oct. 27
    Created react component "Frame"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import './App.css';
 
-import image from "./assets/image.jpg";
+import Frame from "./components/Frame.js";
 
 function App() {
   return (
-    <section class="container">
-      <img src={image} alt="build day" />
-      <div class="text">
-        <h1>We're a High School Robotics Team Based In Westminster CA</h1>
-        <p>Founded in 2020, Team 8521 is a robotics team aiming to provide our community with hands-on opportunities and mentorships. We want to empower our community and give them the chance to show their leader and innovator ability.</p>
-      </div>
-    </section>
+    <div>
+    <Frame />
+    <Frame />
+    <Frame />
+  
+    </div>
   );
 }
 
--- src/components/README.md
+++
@@ -1,3 +0,0 @@
-# Components
-
-Your React components should go here. This is where you're writing the JSX functions to recycle your code.
